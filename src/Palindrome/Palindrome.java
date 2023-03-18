5976978c94ae13938de44f9721f7286a7578d499
DoChuan13 <chuando.9213@gmail.com>
2023-03-18 15:53:36 +0700

    Palindrome Chain Check

--- src/Palindrome/Palindrome.java
+++ src/Palindrome/Palindrome.java
@@ -5,16 +5,27 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Palindrome {
+    static Stack<String> strStack = new Stack<>();
+    static ArrayDeque<String> strQueue = new ArrayDeque<>();
+
     public static void checkPalidrome(Scanner scanner) {
-        Stack<String> strStack = new Stack<>();
-        ArrayDeque<String> strQueue = new ArrayDeque<>();
         System.out.print("Nhập vào một chuỗi ký tự: ");
         String str = scanner.nextLine();
         String[] strArray = str.split("");
+        putValueInStackAndQueue(strArray);
+        boolean flag = checkMatchPalindrome(strArray);
+        if (flag) System.out.println("Đây là chuỗi Palidrome");
+        else System.err.println("Đây KHÔNG phải là chuỗi Palidrome");
+    }
+
+    private static void putValueInStackAndQueue(String[] strArray) {
         for (int i = 0; i < strArray.length; i++) {
             strStack.push(strArray[i]);
             strQueue.add(strArray[i]);
         }
+    }
+
+    private static boolean checkMatchPalindrome(String[] strArray) {
         boolean flag = true;
         for (int i = 0; i < strArray.length; i++) {
             String stackCharacter = strStack.pop();
@@ -26,7 +37,6 @@ public class Palindrome {
                 break;
             }
         }
-        if (flag) System.out.println("Đây là chuỗi Palidrome");
-        else System.err.println("Đây KHÔNG phải là chuỗi Palidrome");
+        return flag;
     }
 }
